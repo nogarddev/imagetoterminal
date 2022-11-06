11eae16c10ab877343230f1ded052b888e6a6b13
nogarddev <115876934+nogarddev@users.noreply.github.com>
2022-11-06 08:32:57 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -15,9 +15,13 @@ public class Main {
         Integer wanted_height = Integer.valueOf(sc.nextLine());
         System.out.println("Enter amount of frames:");
         Integer frames = Integer.valueOf(sc.nextLine());
+        System.out.println("Enter output dir name:");
+        String outputdir = String.valueOf(sc.nextLine());
         Integer currentframe = 0;
         String filename;
         while (currentframe.equals(frames) == false) {
+//            System.out.println("frame.length = " + frame.length);
+//            System.out.println("frame[0].length = " + frame[0].length);
             currentframe++;
             filename = "" + currentframe;
             if (currentframe < 10000) {
@@ -43,7 +47,7 @@ public class Main {
             }
             Integer imagewidth = image.getWidth();
             Integer imageheight = image.getHeight();
-            String[][] frame = new String[wanted_width][wanted_height];
+            String[][] frame = new String[wanted_height][wanted_width];
             Integer x = 0;
             Integer y = 0;
             Integer x_factor = imagewidth / wanted_width;
@@ -92,7 +96,7 @@ public class Main {
                             current_pixel = new Color(image.getRGB(selected_x, selected_y));
                             total_r = total_r + current_pixel.getRed();
                             total_g = total_g + current_pixel.getGreen();
-                            total_b = total_b + current_pixel.getBlue();                
+                            total_b = total_b + current_pixel.getBlue();
                             y1++;
                             selected_y++;
                         }
@@ -104,19 +108,32 @@ public class Main {
                     average_g = total_g / selected_pixels;
                     average_b = total_b / selected_pixels;
                     setpixel((x) / x_factor, (y) / y_factor, average_r, average_g, average_b);
-                    frame[(x) / x_factor][(y) / y_factor] = "[48;2;" + average_r + ";" + average_g + ";" + average_b + "m ";
+                    frame[(y) / y_factor][(x) / x_factor] = "[48;2;" + average_r + ";" + average_g + ";" + average_b + "m ";
                     x = x + x_factor;
     //                System.out.println("average_r = " + average_r);
     //                System.out.println("average_g = " + average_g);
     //                System.out.println("average_b = " + average_b);
-            System.out.print("[1A");
-            System.out.print("[2K");
-            System.out.println(finished_segments + " segments done out of " + total_segments + " segments");
+                    System.out.print("[1A");
+                    System.out.print("[2K");
+                    System.out.println(finished_segments + " segments done out of " + total_segments + " segments");
                 }
                 x = 0;
                 y = y + y_factor;
             }
-            File output = new File("preview.png");
+            long_id = String.valueOf(currentframe);
+            if (currentframe < 10000) {
+                if (currentframe < 1000) {
+                    if (currentframe < 100) {
+                        if (currentframe < 10) {
+                            long_id = "0" + long_id;
+                        }
+                        long_id = "0" + long_id;
+                    }
+                    long_id = "0" + long_id;
+                }
+                long_id = "0" + long_id;
+            }
+            File output = new File("./" + outputdir + "/" + long_id + ".png");
             try {
                 ImageIO.write(preview, "png", output);
             } catch (Exception ex) {
@@ -127,42 +144,43 @@ public class Main {
             System.out.print("[2K");
             System.out.print("[1A");
             System.out.print("[2K");
-            x = 0;
-            y = 0;
-            frameout = new String[frame.length];
-            while (y < frame.length) {
-                frameout[y] = "System.out.println(\"";
-                while (x < frame[y].length) {
-                    frameout[y] = frameout[y] + frame[y][x++];
-                }
-                System.out.println(frameout[y]);
-                frameout[y] = frameout[y] + "\");";
-                y++;
-                x = 0;
-            }
-            try {
-                File myObj = new File("out.txt");
-                if (myObj.createNewFile()) {
-                    skip = 1;
-                }
-            } catch (IOException ex) {
-                //blank
-            }
-            try {
-                FileWriter fw = new FileWriter("out.txt", true);
-                i = 0;
-                fw.write("Sleep(\"17\");\n");
-                while (i.equals(frameout.length) == false) {
-                    fw.write(frameout[i++] + "\n");
-                }
-                fw.write("\n");
-                fw.close();
-            } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }          
+//            x = 0;
+//            y = 0;
+//            frameout = new String[frame.length];
+//            while (y < frame.length) {
+//                frameout[y] = "System.out.println(\"";
+//                while (x < frame[y].length) {
+//                    frameout[y] = frameout[y] + frame[y][x++];
+//                }
+//                System.out.println(frameout[y]);
+//                frameout[y] = frameout[y] + "\");";
+//                y++;
+//                x = 0;
+//            }
+//            try {
+//                File myObj = new File("out.txt");
+//                if (myObj.createNewFile()) {
+//                    skip = 1;
+//                }
+//            } catch (IOException ex) {
+//                //blank
+//            }
+//            try {
+//                FileWriter fw = new FileWriter("out.txt", true);
+//                i = 0;
+//                fw.write("Sleep(\"17\");\n");
+//                while (i.equals(frameout.length) == false) {
+//                    fw.write(frameout[i++] + "\n");
+//                }
+//                fw.write("\n");
+//                fw.close();
+//            } catch (IOException e) {
+//                System.out.println("An error occurred.");
+//                e.printStackTrace();
+//            }          
         }
     }
+    static String long_id;
     static File out;
     static Integer skip;
     static Integer i;
